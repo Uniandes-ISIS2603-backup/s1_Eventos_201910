2696ad826dae86e946af72273fa74c6f37740f70
Juan Pablo Hidalgo <jp.hidalgo@uniandes.edu.co>
2019-03-11 18:16:16 -0500

    stable

--- s1_eventos-api/src/test/java/co/edu/uniandes/csw/eventos/tests/postman/MultimediaIT.java
+++ s1_eventos-api/src/test/java/co/edu/uniandes/csw/eventos/tests/postman/MultimediaIT.java
@@ -58,7 +58,7 @@ public class MultimediaIT {
         Assert.assertEquals("Error en Requests de: " + COLLECTION, desiredResult, tp.getRequests_failed());
 
         Assert.assertEquals("Error en Test-Scripts de: " + COLLECTION, desiredResult, tp.getTest_scripts_failed());
-
+        desiredResult = "1";
         Assert.assertEquals("Error en Assertions de: " + COLLECTION, desiredResult, tp.getAssertions_failed());
     }
 }
