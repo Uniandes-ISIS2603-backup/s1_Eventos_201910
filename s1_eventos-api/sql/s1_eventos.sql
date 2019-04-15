99896db71d385775e763affffc6635de3fbd75e3
Nicolas Diaz <na.diaz@uniandes.edu.co>
2019-04-14 21:35:00 -0500

    Agregar Valores Base de Datos

--- s1_eventos-api/sql/s1_eventos.sql
+++ s1_eventos-api/sql/s1_eventos.sql
@@ -4,7 +4,8 @@ delete from EventoEntity;
 delete from EntradaEntity;
 delete from CalificacionEntity;
 delete from MedioDePagoEntity;
-
+delete from UsuarioEntity;
+delete from MultimediaEntity;
 
 insert into PatrocinadorEntity(id, nombre, imagen, rango, descripcion) values (100, 'El Espectador','https://www.elespectador.com/sites/default/files/f132055d336bdccca7332e9c54da0167.jpg', 'Gold', 'Noticias de Colombia y el mundo. El Espectador, el valor de la Información.' );
 insert into PatrocinadorEntity(id, nombre, imagen, rango, descripcion) values (200, 'Microsoft Corporation', 'https://cdn.vox-cdn.com/thumbor/NeSo4JAqv-fFJCIhb5K5eBqvXG4=/7x0:633x417/1200x800/filters:focal(7x0:633x417)/cdn.vox-cdn.com/assets/1311169/mslogo.jpg', 'Gold', 'Tu potencial, nuestra pasión.');
@@ -39,3 +40,13 @@ insert into MedioDePagoEntity(id, numero, titular, codigoDeSeguridad, fechaDeExp
 insert into MedioDePagoEntity(id, numero, titular, codigoDeSeguridad, fechaDeExpiracion) values (2,'7778654553221901','JUAN ROMAN RIQUELME','123','2021-12-24 00:00:59');
 insert into MedioDePagoEntity(id, numero, titular, codigoDeSeguridad, fechaDeExpiracion) values (3,'2236875791287138','CALORS VIAFARA','793','2019-05-24 00:00:59');
 insert into MedioDePagoEntity(id, numero, titular, codigoDeSeguridad, fechaDeExpiracion) values (4,'5032908625469090','PEDRO PABLO L','111','2021-07-24 00:00:59');
+
+insert into UsuarioEntity(id, correoElectronico, contrasenia, longitud, latitud, unialpino) values (100,'je.perez@unialpes.edu.co','Contrasena123#',-80.745,80.321,1);
+insert into UsuarioEntity(id, correoElectronico, contrasenia, longitud, latitud, unialpino) values (200,'ca.quintero10@unialpes.edu.co','NoEsUnaContrasena123#',-50.745,60.321,1);
+insert into UsuarioEntity(id, correoElectronico, contrasenia, longitud, latitud, unialpino) values (300,'email@hotmail.com','EmailYContrasena123#',-40.745,-40.321,0);
+insert into UsuarioEntity(id, correoElectronico, contrasenia, longitud, latitud, unialpino) values (400,'jlm01010@gmail.com','JoseLopezMartinez123#',-85.745,85.321,0);
+
+insert into MultimediaEntity(id, url, nombre, tipo, memoria) values (100, 'http://managersmagazine.com/wp-content/uploads/2018/06/eventos-y-branding-750x410.jpg','Noche de Fiesta','Reunion',0);
+insert into MultimediaEntity(id, url, nombre, tipo, memoria) values (200, 'https://estatico1.diariolibre.com/binrepository/546x364/0c7/546d350/none/10904/PPPP/3-discoteca-hi-ibiza_11077768_20190101163411.jpg','Baile','Fiesta',1);
+insert into MultimediaEntity(id, url, nombre, tipo, memoria) values (300, 'https://cadenaser00.epimg.net/emisora/imagenes/2018/04/16/radio_madrid/1523886310_620593_1523886468_noticia_normal.jpg','Momento Inolvidable','Reunion',0);
+insert into MultimediaEntity(id, url, nombre, tipo, memoria) values (400, 'https://alianzafrancesa.org.mx/aguascalientes/wp-content/uploads/sites/3/2018/05/unnamed.jpg','Buen momento','Fiesta',1);
