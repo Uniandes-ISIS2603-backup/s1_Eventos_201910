697d3d36f975dd3c2932dce45b8414d1f34dd4ac
Paula Molina <pa.molina@uniandes.edu.co>
2019-05-19 21:33:29 -0500

    Organizador Eventos Test

--- s1_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/logic/OrganizadorEventosLogicTest.java
+++ s1_eventos-back/src/test/java/co/edu/uniandes/csw/eventos/test/logic/OrganizadorEventosLogicTest.java
@@ -1,22 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package co.edu.uniandes.csw.eventos.test.logic;
 
-import co.edu.uniandes.csw.eventos.ejb.UsuarioEventosLogic;
-import co.edu.uniandes.csw.eventos.ejb.UsuarioLogic;
+import co.edu.uniandes.csw.eventos.ejb.OrganizadorEventosLogic;
+import co.edu.uniandes.csw.eventos.ejb.OrganizadorLogic;
 import co.edu.uniandes.csw.eventos.entities.EventoEntity;
-import co.edu.uniandes.csw.eventos.entities.UsuarioEntity;
+import co.edu.uniandes.csw.eventos.entities.OrganizadorEntity;
 import co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException;
-import co.edu.uniandes.csw.eventos.persistence.UsuarioPersistence;
+import co.edu.uniandes.csw.eventos.persistence.OrganizadorPersistence;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
-import org.junit.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,19 +29,19 @@ import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
- * Pruebas de logica de la relacion Usuario - Eventos
  *
- * @author Nicolas Diaz
+ * @author estudiante
  */
 @RunWith(Arquillian.class)
-public class UsuarioEventosLogicTest {
-
+public class OrganizadorEventosLogicTest {
+    
+    
     private PodamFactory factory = new PodamFactoryImpl();
 
     @Inject
-    private UsuarioLogic usuarioLogic;
+    private OrganizadorLogic OrganizadorLogic;
     @Inject
-    private UsuarioEventosLogic usuarioEventosLogic;
+    private OrganizadorEventosLogic OrganizadorEventosLogic;
 
     @PersistenceContext
     private EntityManager em;
@@ -44,16 +49,16 @@ public class UsuarioEventosLogicTest {
     @Inject
     private UserTransaction utx;
 
-    private List<UsuarioEntity> data = new ArrayList<>();
+    private List<OrganizadorEntity> data = new ArrayList<>();
 
     private List<EventoEntity> eventosData = new ArrayList();
 
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addPackage(UsuarioEntity.class.getPackage())
-                .addPackage(UsuarioLogic.class.getPackage())
-                .addPackage(UsuarioPersistence.class.getPackage())
+                .addPackage(OrganizadorEntity.class.getPackage())
+                .addPackage(OrganizadorLogic.class.getPackage())
+                .addPackage(OrganizadorPersistence.class.getPackage())
                 .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
@@ -83,7 +88,7 @@ public class UsuarioEventosLogicTest {
      */
     private void clearData() {
         em.createQuery("delete from EventoEntity").executeUpdate();
-        em.createQuery("delete from UsuarioEntity").executeUpdate();
+        em.createQuery("delete from OrganizadorEntity").executeUpdate();
     }
 
     /**
@@ -97,28 +102,23 @@ public class UsuarioEventosLogicTest {
             eventosData.add(eventos);
         }
         for (int i = 0; i < 3; i++) {
-            UsuarioEntity entity = factory.manufacturePojo(UsuarioEntity.class);
-            entity.setCorreoElectronico("usuario"+i+"@hotmail.com");
-            entity.setContrasenia("aaaaBBBB"+i+"%");
-            entity.setLatitud(-78.65+i);
-            entity.setLongitud(170.98+i);
-            entity.setUnialpino(false);
+            OrganizadorEntity entity = factory.manufacturePojo(OrganizadorEntity.class);
             em.persist(entity);
             data.add(entity);
             if (i == 0) {
-                eventosData.get(i).getUsuarios().add(entity);
+                eventosData.get(i).getOrganizadores().add(entity);
             }
         }
     }
 
     /**
-     * Prueba para asociar un Evento existente a un Usuario.
+     * Prueba para asociar un Evento existente a un Organizador.
      */
     @Test
     public void addEventosTest() {
-        UsuarioEntity entity = data.get(0);
+        OrganizadorEntity entity = data.get(0);
         EventoEntity eventoEntity = eventosData.get(1);
-        EventoEntity response = usuarioEventosLogic.addEvento(eventoEntity.getId(), entity.getId());
+        EventoEntity response = OrganizadorEventosLogic.addEvento(eventoEntity.getId(), entity.getId());
 
         Assert.assertNotNull(response);
         Assert.assertEquals(eventoEntity.getId(), response.getId());
@@ -126,25 +126,25 @@ public class UsuarioEventosLogicTest {
 
     /**
      * Prueba para obtener una colección de instancias de Eventos asociadas a una
-     * instancia Usuario.
+     * instancia Organizador.
      */
     @Test
     public void getEventosTest() {
-        List<EventoEntity> list = usuarioEventosLogic.getEventos(data.get(0).getId());
+        List<EventoEntity> list = OrganizadorEventosLogic.getEventos(data.get(0).getId());
         Assert.assertEquals(1, list.size());
     }
 
     /**
      * Prueba para obtener una instancia de Eventos asociada a una instancia
-     * Usuario.
+     * Organizador.
      *
      * @throws co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException
      */
     @Test
     public void getEventoTest() throws BusinessLogicException {
-        UsuarioEntity entity = data.get(0);
+        OrganizadorEntity entity = data.get(0);
         EventoEntity eventoEntity = eventosData.get(0);
-        EventoEntity response = usuarioEventosLogic.getEvento(entity.getId(), eventoEntity.getId());
+        EventoEntity response = OrganizadorEventosLogic.getEvento(entity.getId(), eventoEntity.getId());
 
         Assert.assertEquals(eventoEntity.getId(), response.getId());
         Assert.assertEquals(eventoEntity.getNombre(), response.getNombre());
@@ -155,43 +155,43 @@ public class UsuarioEventosLogicTest {
 
     /**
      * Prueba para obtener una instancia de Eventos asociada a una instancia
-     * Usuario que no le pertenece.
+     * Organizador que no le pertenece.
      *
      * @throws co.edu.uniandes.csw.eventos.exceptions.BusinessLogicException
      */
     @Test(expected = BusinessLogicException.class)
     public void getEventoNoAsociadoTest() throws BusinessLogicException {
-        UsuarioEntity entity = data.get(0);
+        OrganizadorEntity entity = data.get(0);
         EventoEntity eventoEntity = eventosData.get(1);
-        usuarioEventosLogic.getEvento(entity.getId(), eventoEntity.getId());
+        OrganizadorEventosLogic.getEvento(entity.getId(), eventoEntity.getId());
     }
 
     /**
      * Prueba para remplazar las instancias de Eventos asociadas a una instancia
-     * de Usuario.
+     * de Organizador.
      */
     @Test
     public void replaceEventosTest() {
-        UsuarioEntity entity = data.get(0);
+        OrganizadorEntity entity = data.get(0);
         List<EventoEntity> list = eventosData.subList(1, 3);
-        usuarioEventosLogic.replaceEventos(entity.getId(), list);
+        OrganizadorEventosLogic.replaceEventos(entity.getId(), list);
 
-        entity = usuarioLogic.getUsuario(entity.getId());
+        entity = OrganizadorLogic.getOrganizador(entity.getId());
         Assert.assertFalse(entity.getEventos().contains(eventosData.get(0)));
         Assert.assertTrue(entity.getEventos().contains(eventosData.get(1)));
         Assert.assertTrue(entity.getEventos().contains(eventosData.get(2)));
     }
     
     /**
-     * Prueba desasociar un evento con un usuario.
+     * Prueba desasociar un evento con un Organizador.
      *
      */
     @Test
     public void removeEventoTest() {
-        UsuarioEntity entity = data.get(0);
+        OrganizadorEntity entity = data.get(0);
         for (EventoEntity evento : eventosData) {
-            usuarioEventosLogic.removeEvento(entity.getId(), evento.getId());
+            OrganizadorEventosLogic.removeEvento(entity.getId(), evento.getId());
         }
-        Assert.assertTrue(usuarioEventosLogic.getEventos(entity.getId()).isEmpty());
+        Assert.assertTrue(OrganizadorEventosLogic.getEventos(entity.getId()).isEmpty());
     }
 }
